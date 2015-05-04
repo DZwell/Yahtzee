80790a25bdaae2f0f420d005e18f0e9ae0c5c567
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-05-03 22:06:12 -0700

    Built table, appended dice rolls to it

--- app.js
+++ app.js
@@ -8,11 +8,22 @@ for (var i = 0; i < 5; i++) {
   myArr[i] = new Dice(Dice.roll);
 }
 
-// local storage
+// save to session storage
 
 sessionStorage['myArr'] = JSON.stringify(myArr);
-var storedArry = JSON.parse(sessionStorage['myArr']);
-console.log(sessionStorage);
+var storedArray = JSON.parse(sessionStorage['myArr']);
+console.log(storedArray[0].roll);
+console.log(storedArray[1].roll);
+console.log(storedArray[2].roll);
+console.log(storedArray[3].roll);
+console.log(storedArray[4].roll);
+
+
+
+//retrive from session storage
+
+
+
 
 // reload button
 // adds new empty array and creates 5 new dice
